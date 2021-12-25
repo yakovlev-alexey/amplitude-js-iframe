1e85827f3df5b1b9c793af790cd09ac489b97165
Alexey Yakovlev <alexeyyakovlev0359@gmail.com>
2021-12-25 17:18:01 +0300

    feat: add util to get iframe message port by selector

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,43 @@
 import { DOM_LOADED_STATES } from "./constants";
 
-export const isDomContentLoaded = () => {
-  return !window || DOM_LOADED_STATES.includes(document.readyState);
+const isDomContentLoaded = (document = window.document) => {
+  return DOM_LOADED_STATES.includes(document.readyState);
+};
+
+const isIframe = (element: Element | null): element is HTMLIFrameElement =>
+  element instanceof HTMLIFrameElement;
+
+export const getIframeMessagePort = async (
+  iframeSelector: string
+): Promise<WindowProxy> => {
+  return new Promise((ok, no) => {
+    if (isDomContentLoaded()) {
+      const iframe = document.querySelector(iframeSelector);
+
+      if (!isIframe(iframe)) {
+        no(
+          new Error(
+            `Provided selector ${iframeSelector} doesn't yield an iframe`
+          )
+        );
+      }
+
+      ok((iframe as HTMLIFrameElement).contentWindow!);
+      return;
+    }
+
+    document.addEventListener("DOMContentLoaded", () => {
+      const iframe = document.querySelector(iframeSelector);
+
+      if (!isIframe(iframe)) {
+        no(
+          new Error(
+            `Provided selector ${iframeSelector} doesn't yield an iframe`
+          )
+        );
+      }
+
+      ok((iframe as HTMLIFrameElement).contentWindow!);
+    });
+  });
 };
