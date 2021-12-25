95ff3faee6d1e0d50bd10fcf0cc04c83aef251c3
Alexey Yakovlev <alexeyyakovlev0359@gmail.com>
2021-12-25 17:27:49 +0300

    feat: add linters and format code

--- src/utils.ts
+++ src/utils.ts
@@ -1,43 +1,37 @@
 import { DOM_LOADED_STATES } from "./constants";
 
 const isDomContentLoaded = (document = window.document) => {
-  return DOM_LOADED_STATES.includes(document.readyState);
+    return DOM_LOADED_STATES.includes(document.readyState);
 };
 
 const isIframe = (element: Element | null): element is HTMLIFrameElement =>
-  element instanceof HTMLIFrameElement;
+    element instanceof HTMLIFrameElement;
 
-export const getIframeMessagePort = async (
-  iframeSelector: string
-): Promise<WindowProxy> => {
-  return new Promise((ok, no) => {
-    if (isDomContentLoaded()) {
-      const iframe = document.querySelector(iframeSelector);
+export const getIframeMessagePort = async (iframeSelector: string): Promise<WindowProxy> => {
+    return new Promise((ok, no) => {
+        if (isDomContentLoaded()) {
+            const iframe = document.querySelector(iframeSelector);
 
-      if (!isIframe(iframe)) {
-        no(
-          new Error(
-            `Provided selector ${iframeSelector} doesn't yield an iframe`
-          )
-        );
-      }
+            if (!isIframe(iframe)) {
+                no(new Error(`Provided selector ${iframeSelector} doesn't yield an iframe`));
+            }
 
-      ok((iframe as HTMLIFrameElement).contentWindow!);
-      return;
-    }
+            // iframe is returned by query selector therefore is in DOM
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            ok((iframe as HTMLIFrameElement).contentWindow!);
+            return;
+        }
 
-    document.addEventListener("DOMContentLoaded", () => {
-      const iframe = document.querySelector(iframeSelector);
+        document.addEventListener("DOMContentLoaded", () => {
+            const iframe = document.querySelector(iframeSelector);
 
-      if (!isIframe(iframe)) {
-        no(
-          new Error(
-            `Provided selector ${iframeSelector} doesn't yield an iframe`
-          )
-        );
-      }
+            if (!isIframe(iframe)) {
+                no(new Error(`Provided selector ${iframeSelector} doesn't yield an iframe`));
+            }
 
-      ok((iframe as HTMLIFrameElement).contentWindow!);
+            // iframe is returned by query selector therefore is in DOM
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            ok((iframe as HTMLIFrameElement).contentWindow!);
+        });
     });
-  });
 };
